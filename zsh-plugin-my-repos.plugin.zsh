b49b0f3fa6e1e05edbc8fded40626fedec2597f5
Aubrey Portwood <code@aubreypwd.com>
2022-04-19 14:10:54 -0600

    Update to include my antigen repos.

--- zsh-plugin-my-repos.plugin.zsh
+++ zsh-plugin-my-repos.plugin.zsh
@@ -24,17 +24,25 @@ function __dirty_message {
  #
  # @since Friday, August 27, 2021
  ##
-function __git-is-clean {
+function __watchrepo {
 	git-is-clean "$1" || ( __dirty_message "$1" )
 }
 
 # Watch these repositories for dirtiness.
-__git-is-clean "$HOME/Repos/github.com/aubreypwd/iTerm2"
-__git-is-clean "$HOME/Repos/github.com/aubreypwd/Alfred.alfredpreferences"
-__git-is-clean "$HOME/Repos/github.com/aubreypwd/subl-snippets"
-__git-is-clean "$HOME/Repos/github.com/aubreypwd/safari-user-scripts"
-__git-is-clean "$HOME/iCloud/Profile Photos"
-__git-is-clean "$HOME/.antigen/bundles/aubreypwd/zsh-plugin-my-aliases"
+__watchrepo "$HOME/Repos/github.com/aubreypwd/iTerm2"
+__watchrepo "$HOME/Repos/github.com/aubreypwd/Alfred.alfredpreferences"
+__watchrepo "$HOME/Repos/github.com/aubreypwd/subl-snippets"
+__watchrepo "$HOME/Repos/github.com/aubreypwd/safari-user-scripts"
+__watchrepo "$HOME/iCloud/Profile Photos"
+
+# My ZSH plugins/configurations.
+__watchrepo "$HOME/.antigen/bundles/aubreypwd/zsh-plugin-my-aliases"
+__watchrepo "$HOME/.antigen/bundles/aubreypwd/zsh-plugin-my-functions"
+__watchrepo "$HOME/.antigen/bundles/aubreypwd/zsh-plugin-my-mac-defaults"
+__watchrepo "$HOME/.antigen/bundles/aubreypwd/zsh-plugin-my-opts"
+__watchrepo "$HOME/.antigen/bundles/aubreypwd/zsh-plugin-my-repos"
+__watchrepo "$HOME/.antigen/bundles/aubreypwd/zsh-plugin-my-require"
+__watchrepo "$HOME/.antigen/bundles/aubreypwd/zsh-plugin-my-vars"
 
 if [[ ! $( command -v vcsh ) ]]; then
 
@@ -44,5 +52,4 @@ else
 	# Have to do something special here for vcsh.
 	vcsh pub diff-index --quiet --ignore-submodules HEAD || __dirty_message "pub"
 	vcsh priv diff-index --quiet --ignore-submodules HEAD || __dirty_message "priv"
-
 fi
